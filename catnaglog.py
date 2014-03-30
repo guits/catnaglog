ab0baae4db19935e43c9dda23565acc0b0b12235
Guits <guillaume.abrioux@enovance.com>
2014-03-30 16:47:11 +0200

    Add Possibility to specify the path to the log file

--- catnaglog.py
+++ catnaglog.py
@@ -2,23 +2,35 @@
 
 __author__ = 'guits'
 
-import os
-import sys
 import time
 import re
+import argparse
 
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument("-f", "--file",
+            help="Path to the log file (Default: ./nagios.log)", type=str, required=False)
 
-filename = './nagios.log'
+ARGS = PARSER.parse_args()
 
-f = open(filename, 'r')
+if ARGS.file:
+    FILENAME = ARGS.file
+else:
+    FILENAME = './nagios.log'
 
 try:
-    for line in f:
-        m = re.match('^\[([^\]]+)\] (.+)$', line)
+    F = open(FILENAME, 'r')
+except IOError:
+    print "Can't open file."
+    exit(-1)
+
+try:
+    for line in F:
+        m = re.match(r'^\[([^\]]+)\] (.+)$', line)
         if re is not None:
             timestamp = m.group(1)
             infos = m.group(2)
-            hdate = time.strftime('%d/%m/%Y %H:%M:%S', time.localtime(int(timestamp)))
+            hdate = time.strftime('%d/%m/%Y %H:%M:%S',
+                                  time.localtime(int(timestamp)))
             print "[%s] %s" % (hdate, infos)
 except (KeyboardInterrupt, IOError):
     pass
